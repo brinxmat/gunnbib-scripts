f371f566eb3ad21ba6eec79482b72275f02f3e4d
Rurik Greenall <rurik.greenall@gmail.com>
2015-04-17 13:58:52 +0200

    Add triple management & data reader

--- lib/munger.rb
+++ lib/munger.rb
@@ -4,21 +4,19 @@
 require 'rdf'
 require 'rdf/ntriples'
 require 'test/unit'
+require 'csv'
 
 class Munger
 
   @@model = nil
+  @@baseURI = nil
 
   def initialize ()
     create_new_model
   end
 
   def create_new_model ()
-    if (@@model == nil)
       @@model = RDF::Graph.new
-    else
-      STDOUT.puts "Attempted to initialize model, but a pre-existing model was found"
-    end
   end
 
   def add_triple (s, p, o)
@@ -33,4 +31,46 @@ class Munger
     @@model.dump(:ntriples)
   end
 
+  def read_data_from_file (file)
+    @data = {}
+    CSV.foreach(file,
+                 {:headers => true, :header_converters => :symbol}
+                ) do |row|
+      @data.merge!(row)
+    end
+
+    return @data
+  end
+
+  def set_base_uri (uri)
+    @@baseURI = uri
+  end
+
+  def get_base_uri ()
+    return @@baseURI
+  end
+
+  def add_creator (creator)
+    add_triple(
+      get_base_uri,
+      RDF::URI.new("http://purl.org/dc/terms/creator"),
+      RDF::URI.new("http://www.ntnu.no/ub/digital/person/" + creator)
+      )
+  end
+
+  def add_title (title)
+    add_triple(
+      get_base_uri,
+      RDF::URI.new("http://purl.org/dc/terms/title"),
+      RDF::Literal.new(title)
+      )
+  end
+
+    def add_identifier (identifier)
+    add_triple(
+      get_base_uri,
+      RDF::URI.new("http://purl.org/dc/terms/identifier"),
+      RDF::Literal.new(identifier)
+      )
+  end
 end
--- test/test_munger.rb
+++ test/test_munger.rb
@@ -23,15 +23,74 @@ class TestMunger < Test::Unit::TestCase
       output.has_statement?(
         RDF::Statement.new(@@s, @@p, @@o)
       ), 
-    "Model did not contain expected work data")
+    "Model did not contain expected triple")
   end
 
-  def test_get_model_as_ntriples
+  def test_get_model_as_ntriples ()
     test_model = RDF::Graph.new << [@@s, @@p, @@o]
     model = Munger.new
     model.add_triple(@@s, @@p, @@o)
     output = model.get_model_as_ntriples
     assert_equal(test_model.dump(:ntriples), output)
+  end
+
+  def test_read_data_from_file ()
+    munger = Munger.new
+    output = munger.read_data_from_file('test/data/simple.csv')
+    data = {:field_0 => 'zero', 
+            :field_1 => 'one'}
+    assert_equal(data, output)
+  end
+
+  def test_set_base_uri ()
+    munger = Munger.new
+    munger.set_base_uri(@@s)
+    assert_equal(munger.get_base_uri, @@s)
+  end
+
+  def test_add_creator ()
+    p_creator = RDF::URI.new("http://purl.org/dc/terms/creator")
+    o_creator = RDF::URI.new("http://www.ntnu.no/ub/digital/person/f1")
+    munger = Munger.new
+    munger.set_base_uri(@@s)
+    munger.add_creator("f1")
+    model = munger.get_model
+
+    assert(
+      model.has_statement?(
+        RDF::Statement.new(@@s, p_creator, o_creator)
+      ), 
+    "Model did not contain expected creator triple")
+  end
+
+  def test_add_title ()
+    p_title = RDF::URI.new("http://purl.org/dc/terms/title")
+    o_title = "t1"
+    munger = Munger.new
+    munger.set_base_uri(@@s)
+    munger.add_title("t1")
+    model = munger.get_model
+
+    assert(
+      model.has_statement?(
+        RDF::Statement.new(@@s, p_title, o_title)
+      ), 
+    "Model did not contain expected title triple")
+  end
+
+  def test_add_identifier ()
+    p_identifier = RDF::URI.new("http://purl.org/dc/terms/identifier")
+    o_identifier = "i1"
+    munger = Munger.new
+    munger.set_base_uri(@@s)
+    munger.add_identifier("i1")
+    model = munger.get_model
+
+    assert(
+      model.has_statement?(
+        RDF::Statement.new(@@s, p_identifier, o_identifier)
+      ), 
+    "Model did not contain expected identifier triple")
   end
 
 end
