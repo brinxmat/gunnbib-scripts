8ff504900b218e225037559f7a4a911d3c615a47
Rurik Greenall <rurik.greenall@gmail.com>
2015-04-18 20:17:18 +0200

    Add more tests and functionality

--- lib/munger.rb
+++ lib/munger.rb
@@ -5,6 +5,9 @@ require 'rdf'
 require 'rdf/ntriples'
 require 'test/unit'
 require 'csv'
+require 'uuid'
+require 'date'
+require 'securerandom'
 
 class Munger
 
@@ -16,7 +19,7 @@ class Munger
   end
 
   def create_new_model ()
-      @@model = RDF::Graph.new
+      @@model = RDF::Repository.new
   end
 
   def add_triple (s, p, o)
@@ -66,11 +69,238 @@ class Munger
       )
   end
 
-    def add_identifier (identifier)
+  def add_identifier (identifier)
     add_triple(
       get_base_uri,
       RDF::URI.new("http://purl.org/dc/terms/identifier"),
       RDF::Literal.new(identifier)
       )
   end
+
+  def add_recipient (recipient)
+    add_triple(
+      get_base_uri,
+      RDF::URI.new("http://purl.org/ontology/bibo/recipient"),
+      RDF::URI.new("http://www.ntnu.no/ub/digital/person/" + recipient)
+      )
+  end
+
+  def add_document_type (type)
+    add_triple(
+      get_base_uri,
+      RDF::URI.new("http://www.w3.org/1999/02/22-rdf-syntax-ns#type"),
+      RDF::URI.new("http://www.ntnu.no/ub/digital/format/" + type)
+      )
+  end
+
+  def add_country (country)
+    add_triple(
+      get_base_uri,
+      RDF::URI.new("http://www.ntnu.no/ub/data/nl#geoSubjectCountry"),
+      RDF::URI.new("http://sws.geonames.org/" + country)
+      )
+  end
+
+  def add_county (county)
+    add_triple(
+      get_base_uri,
+      RDF::URI.new("http://www.ntnu.no/ub/data/nl#geoSubjectCounty"),
+      RDF::URI.new("http://sws.geonames.org/" + county)
+      )
+  end
+
+  def add_municipality (municipality)
+    add_triple(
+      get_base_uri,
+      RDF::URI.new("http://www.ntnu.no/ub/data/nl#geoSubjectMunicipality"),
+      RDF::URI.new("http://sws.geonames.org/" + municipality)
+      )
+  end
+
+    def add_place_of_creation (place)
+    add_triple(
+      get_base_uri,
+      RDF::URI.new("http://www.ntnu.no/ub/data/nl#placeOfCreation"),
+      RDF::URI.new("http://sws.geonames.org/" + place)
+      )
+  end
+
+  def add_genre (genre)
+    add_triple(
+      get_base_uri,
+      RDF::URI.new("http://www.ntnu.no/ub/data/nl#genre"),
+      RDF::URI.new("http://www.ntnu.no/ub/data/genre/" + genre)
+      )
+  end
+
+  def add_text_subject (subject)
+    add_triple(
+      get_base_uri,
+      RDF::URI.new("http://purl.org/dc/terms/subject"),
+      RDF::Literal.new(subject)
+      )
+  end
+
+  def add_subject (subject)
+    add_triple(
+      get_base_uri,
+      RDF::URI.new("http://purl.org/dc/terms/subject"),
+      RDF::URI.new("http://www.example.com/" + subject)
+      )
+  end
+
+  def add_map_scale (scale)
+    add_triple(
+      get_base_uri,
+      RDF::URI.new("http://www.geographicknowledge.de/vocab/maps#hasScale"),
+      RDF::Literal.new(scale)
+      )
+  end
+
+  def add_dimensions (dimensions)
+    add_triple(
+      get_base_uri,
+      RDF::URI.new("http://www.ntnu.no/ub/data/nl#dimensions"),
+      RDF::Literal.new(dimensions)
+      )
+  end
+
+  def add_note (note)
+    add_triple(
+      get_base_uri,
+      RDF::URI.new("http://www.ntnu.no/ub/data/nl#note"),
+      RDF::Literal.new(note)
+      )
+  end
+
+  def create_bnode ()
+    bnode = RDF::Node.new(SecureRandom.uuid)
+    return bnode
+  end
+
+  def parse_date (date)
+    d = nil
+    if date =~ /^[0-9]{4}$/
+      d = date
+    elsif date
+      d = Date.parse date
+    end
+    return d
+  end
+
+  def uncertain (date)
+    return date.include? "?"
+  end
+
+  def add_owl_time_instant (bnode, date)
+    d = parse_date date
+
+    if (d.is_a?(Date))
+      year = d.year.to_s
+      month = d.month.to_s
+      day = d.day.to_s
+    else
+      year = d
+    end
+
+    add_triple(
+      bnode,
+      RDF::URI.new("http://www.w3.org/1999/02/22-rdf-syntax-ns#type"),
+      RDF::URI.new("http://www.w3.org/2006/time#Instant")
+    )
+    add_triple(
+      bnode,
+      RDF::URI.new("http://www.w3.org/2006/time#year"),
+      RDF::Literal.new(year, :datatype => RDF::XSD.gYear)
+    )
+
+    if (month != nil)
+      add_triple(
+        bnode,
+        RDF::URI.new("http://www.w3.org/2006/time#month"),
+        RDF::Literal.new("--" + month, :datatype => RDF::XSD.gMonth)
+      )
+      add_triple(
+        bnode,
+        RDF::URI.new("http://www.w3.org/2006/time#day"),
+        RDF::Literal.new("---" + day, :datatype => RDF::XSD.gDay)
+      )
+    end
+
+  end
+
+  def test_certainty (date)
+    certainty = 10
+    if (uncertain(date))
+       date = date.tr('?','')
+       certainty = 5
+    end
+    return {:date => date, :certainty => certainty}
+  end
+
+  def add_point_date (date)
+    
+    d = test_certainty(date)
+
+    bnode = create_bnode
+
+    add_triple(
+      get_base_uri,
+      RDF::URI.new("http://purl.org/dc/terms/date"),
+      bnode
+    )
+
+    add_owl_time_instant(bnode, d[:date].to_s)
+    add_triple(
+      bnode,
+      RDF::URI.new("http://www.ntnu.no/ub/data/nl#certainty"),
+      RDF::Literal.new(d[:certainty].to_s, :datatype => RDF::XSD.integer)
+    )
+  end
+
+  def add_interval_date (date)
+    d = test_certainty(date)
+
+    date1, date2 = d[:date].split(/–/u)
+
+    bnode = create_bnode()
+    beginNode = create_bnode()
+    endNode = create_bnode()
+
+    add_triple(
+      get_base_uri,
+      RDF::URI.new("http://purl.org/dc/terms/date"),
+      bnode
+    )
+
+    add_triple(
+      bnode,
+      RDF::URI.new("http://www.w3.org/1999/02/22-rdf-syntax-ns#type"),
+      RDF::URI.new("http://www.w3.org/2006/time#Interval")
+    )
+
+    add_triple(
+      bnode,
+      RDF::URI.new("http://www.w3.org/2006/time#hasBeginning"),
+      beginNode
+    )
+    add_owl_time_instant(beginNode, date1)
+
+    add_triple(
+      bnode,
+      RDF::URI.new("http://www.w3.org/2006/time#hasEnd"),
+      endNode
+    )
+    add_owl_time_instant(endNode, date2)
+
+    add_triple(
+      bnode,
+      RDF::URI.new("http://www.ntnu.no/ub/data/nl#certainty"),
+      RDF::Literal.new(d[:certainty].to_s, :datatype => RDF::XSD.integer)
+    )
+   
+  end
+
+
+
 end
--- test/test_munger.rb
+++ test/test_munger.rb
@@ -4,6 +4,8 @@
 require 'test/unit'
 require "munger"
 require 'rdf'
+require 'uuid'
+require 'rdf/isomorphic'
 
 class TestMunger < Test::Unit::TestCase
   @@s = RDF::URI.new("http://example.com/d1")
@@ -93,4 +95,254 @@ class TestMunger < Test::Unit::TestCase
     "Model did not contain expected identifier triple")
   end
 
+  def test_add_recipient ()
+    p_recipient = RDF::URI.new("http://purl.org/ontology/bibo/recipient")
+    o_recipient = RDF::URI.new("http://www.ntnu.no/ub/digital/person/f1")
+    munger = Munger.new
+    munger.set_base_uri(@@s)
+    munger.add_recipient("f1")
+    model = munger.get_model
+
+    assert(
+      model.has_statement?(
+        RDF::Statement.new(@@s, p_recipient, o_recipient)
+      ), 
+    "Model did not contain expected recipient triple")
+  end
+
+  def test_add_document_type ()
+    p_format = RDF::URI.new("http://www.w3.org/1999/02/22-rdf-syntax-ns#type")
+    o_format = RDF::URI.new("http://www.ntnu.no/ub/digital/format/f1")
+    munger = Munger.new
+    munger.set_base_uri(@@s)
+    munger.add_document_type("f1")
+    model = munger.get_model
+
+    assert(
+      model.has_statement?(
+        RDF::Statement.new(@@s, p_format, o_format)
+      ), 
+    "Model did not contain expected format triple")
+  end  
+
+  def test_add_country ()
+    p_country = RDF::URI.new("http://www.ntnu.no/ub/data/nl#geoSubjectCountry")
+    o_country = RDF::URI.new("http://sws.geonames.org/f1")
+    munger = Munger.new
+    munger.set_base_uri(@@s)
+    munger.add_country("f1")
+    model = munger.get_model
+
+    assert(
+      model.has_statement?(
+        RDF::Statement.new(@@s, p_country, o_country)
+      ), 
+    "Model did not contain expected country triple")
+  end 
+
+  def test_add_county ()
+    p_county = RDF::URI.new("http://www.ntnu.no/ub/data/nl#geoSubjectCounty")
+    o_county = RDF::URI.new("http://sws.geonames.org/f1")
+    munger = Munger.new
+    munger.set_base_uri(@@s)
+    munger.add_county("f1")
+    model = munger.get_model
+
+    assert(
+      model.has_statement?(
+        RDF::Statement.new(@@s, p_county, o_county)
+      ), 
+    "Model did not contain expected county triple")
+  end 
+
+  def test_add_municipality ()
+    p_municipality = RDF::URI.new("http://www.ntnu.no/ub/data/nl#geoSubjectMunicipality")
+    o_municipality = RDF::URI.new("http://sws.geonames.org/f1")
+    munger = Munger.new
+    munger.set_base_uri(@@s)
+    munger.add_municipality("f1")
+    model = munger.get_model
+
+    assert(
+      model.has_statement?(
+        RDF::Statement.new(@@s, p_municipality, o_municipality)
+      ), 
+    "Model did not contain expected municipality triple")
+  end
+
+  def test_add_place_of_creation ()
+    p_place_of_creation = RDF::URI.new("http://www.ntnu.no/ub/data/nl#placeOfCreation")
+    o_place_of_creation = RDF::URI.new("http://sws.geonames.org/f1")
+    munger = Munger.new
+    munger.set_base_uri(@@s)
+    munger.add_place_of_creation("f1")
+    model = munger.get_model
+
+    assert(
+      model.has_statement?(
+        RDF::Statement.new(@@s, p_place_of_creation, o_place_of_creation)
+      ), 
+    "Model did not contain expected place of creation triple")
+  end 
+
+  def test_add_genre ()
+    p_genre = RDF::URI.new("http://www.ntnu.no/ub/data/nl#genre")
+    o_genre = RDF::URI.new("http://www.ntnu.no/ub/data/genre/g1")
+    munger = Munger.new
+    munger.set_base_uri(@@s)
+    munger.add_genre("g1")
+    model = munger.get_model
+
+    assert(
+      model.has_statement?(
+        RDF::Statement.new(@@s, p_genre, o_genre)
+      ), 
+    "Model did not contain expected genre triple")
+  end
+
+  def test_add_text_subject ()
+    p_text_subject = RDF::URI.new("http://purl.org/dc/terms/subject")
+    o_text_subject = RDF::Literal.new("test")
+    munger = Munger.new
+    munger.set_base_uri(@@s)
+    munger.add_text_subject("test")
+    model = munger.get_model
+
+    assert(
+      model.has_statement?(
+        RDF::Statement.new(@@s, p_text_subject, o_text_subject)
+      ), 
+    "Model did not contain expected text subject triple")
+  end
+
+  def test_add_subject ()
+    p_subject = RDF::URI.new("http://purl.org/dc/terms/subject")
+    o_subject = RDF::URI.new("http://www.example.com/subject")
+    munger = Munger.new
+    munger.set_base_uri(@@s)
+    munger.add_subject("subject")
+    model = munger.get_model
+
+    assert(
+      model.has_statement?(
+        RDF::Statement.new(@@s, p_subject, o_subject)
+      ), 
+    "Model did not contain expected subject triple")
+  end
+
+  def test_add_map_scale ()
+    p_map_scale = RDF::URI.new("http://www.geographicknowledge.de/vocab/maps#hasScale")
+    o_map_scale = RDF::Literal.new("test")
+    munger = Munger.new
+    munger.set_base_uri(@@s)
+    munger.add_map_scale("test")
+    model = munger.get_model
+
+    assert(
+      model.has_statement?(
+        RDF::Statement.new(@@s, p_map_scale, o_map_scale)
+      ), 
+    "Model did not contain expected map scale triple")
+  end
+
+  def test_add_dimensions ()
+    p_dimensions = RDF::URI.new("http://www.ntnu.no/ub/data/nl#dimensions")
+    o_dimensions = RDF::Literal.new("20 x 20")
+    munger = Munger.new
+    munger.set_base_uri(@@s)
+    munger.add_dimensions("20 x 20")
+    model = munger.get_model
+
+    assert(
+      model.has_statement?(
+        RDF::Statement.new(@@s, p_dimensions, o_dimensions)
+      ), 
+    "Model did not contain expected dimensions triple")
+  end
+
+  def test_add_note ()
+    p_note = RDF::URI.new("http://www.ntnu.no/ub/data/nl#note")
+    o_note = RDF::Literal.new("Note")
+    munger = Munger.new
+    munger.set_base_uri(@@s)
+    munger.add_note("Note")
+    model = munger.get_model
+
+    assert(
+      model.has_statement?(
+        RDF::Statement.new(@@s, p_note, o_note)
+      ), 
+    "Model did not contain expected note triple")
+  end
+
+  def test_add_point_date ()
+    repo = RDF::Repository.load('./test/data/simple_point_date.nt')
+    munger = Munger.new
+    munger.set_base_uri(@@s)
+    munger.add_point_date("2015-04-17")
+    output = munger.get_model
+    isomorphic = repo.isomorphic_with? output
+    assert(isomorphic == true, "The round-tripped point-date models were not the same: #{isomorphic}")
+  end
+
+  def test_add_point_date_uncertain ()
+    repo = RDF::Repository.load('./test/data/simple_point_date_uncertain.nt')
+    munger = Munger.new
+    munger.set_base_uri(@@s)
+    munger.add_point_date("2015-04-17?")
+    output = munger.get_model
+    isomorphic = repo.isomorphic_with? output
+    assert(isomorphic == true, "The round-tripped point-date models were not the same: #{isomorphic}")
+  end
+
+  def test_add_point_date_year ()
+    repo = RDF::Repository.load('./test/data/simple_point_date_year.nt')
+    munger = Munger.new
+    munger.set_base_uri(@@s)
+    munger.add_point_date("2015")
+    output = munger.get_model
+    isomorphic = repo.isomorphic_with? output
+    assert(isomorphic == true, "The round-tripped point-date models were not the same: #{isomorphic}")
+  end
+
+  def test_add_span_year
+    repo = RDF::Repository.load('./test/data/simple_interval_year.nt')
+    munger = Munger.new
+    munger.set_base_uri(@@s)
+    munger.add_interval_date("2014–2015")
+    output = munger.get_model
+    isomorphic = repo.isomorphic_with? output
+    assert(isomorphic == true, "The round-tripped span-date models were not the same: #{isomorphic}")
+  end
+
+  def test_add_span_year_uncertain
+    repo = RDF::Repository.load('./test/data/simple_interval_year_uncertain.nt')
+    munger = Munger.new
+    munger.set_base_uri(@@s)
+    munger.add_interval_date("2014–2015?")
+    output = munger.get_model
+    isomorphic = repo.isomorphic_with? output
+    assert(isomorphic == true, "The round-tripped span-date models were not the same: #{isomorphic}")
+  end
+
+  def test_add_span_date
+    repo = RDF::Repository.load('./test/data/simple_interval_date.nt')
+    munger = Munger.new
+    munger.set_base_uri(@@s)
+    munger.add_interval_date("2014-01-02–2015-01-02")
+    output = munger.get_model
+    STDOUT.puts output.dump(:ntriples)
+    isomorphic = repo.isomorphic_with? output
+    assert(isomorphic == true, "The round-tripped span-date models were not the same: #{isomorphic}")
+  end
+  def test_add_span_date_uncertain
+    repo = RDF::Repository.load('./test/data/simple_interval_date_uncertain.nt')
+    munger = Munger.new
+    munger.set_base_uri(@@s)
+    munger.add_interval_date("2014-01-02–2015-01-02?")
+    output = munger.get_model
+    STDOUT.puts output.dump(:ntriples)
+    isomorphic = repo.isomorphic_with? output
+    assert(isomorphic == true, "The round-tripped span-date models were not the same: #{isomorphic}")
+  end
 end
